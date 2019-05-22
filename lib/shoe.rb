2f0bc9a31eebc07165cd8868c523d917fe61e56f
Denali Balser <denali.balser167@gmail.com>
2019-05-22 01:24:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,12 +6,15 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
-
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
   end
+
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
 end
